52d9c3d7a7503dc8e353086ad5318e9b73f13da3
vladmoroca <vladmoroca@gmail.com>
2023-03-16 19:30:19 +0200

    Add test cases and status check

--- implementation_test.go
+++ implementation_test.go
@@ -7,14 +7,43 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestPostfixToPrefix(t *testing.T) { TestingT(t) }
 
-type MySuite struct{}
+type PostfixToPrefixSuite struct{}
 
-var _ = Suite(&MySuite{})
+var _ = Suite(&PostfixToPrefixSuite{})
 
-func (s *MySuite) TestPostfixToPrefix(c *C) {
-    c.Assert("AAA", Equals, "AAA")
+func (s *PostfixToPrefixSuite) TestPostfixToPrefix(c *C) {
+	tests := []struct {
+		name   string
+		input  string
+		output string
+		err    error
+	}{
+		{"short", "2 2 +", "+ 2 2", nil},
+		{"long", "4 2 - 3 * 5 +", "+ * - 4 2 3 5", nil},
+		{"complex", "2 20 * 2 / 3 4 + 3 2 ^ * + 6 - 15 +", "+ - + / * 2 20 2 * + 3 4 ^ 3 2 6 15", nil},
+		{"empty", "", "", InvalidExpressionError{}},
+		{"not enough operators", "2 5 7 +", "", InvalidExpressionError{}},
+		{"too many operators", "- + / * 1 2 3", "", InvalidExpressionError{}},
+		{"invalid token", " \\ 2 2", "", InvalidExpressionError{}},
+	}
+
+	for _, test := range tests {
+
+		c.Logf("CASE: %s", test.name)
+
+		output, err := PostfixToPrefix(test.input)
+
+		status := c.Check(output, Equals, test.output)
+		status = status && c.Check(err, DeepEquals, test.err)
+
+		if status {
+			c.Logf("PASSED\n")
+		} else {
+			c.Logf("FAILED\n")
+		}
+	}
 }
 
 func ExamplePostfixToPrefix() {
